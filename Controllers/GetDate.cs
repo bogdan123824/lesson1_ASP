e923b4f509fbfaaf9e534df117b301e42d731ec1
bogdan123824 <156796479+bogdan123824@users.noreply.github.com>
2024-07-22 21:55:48 +0300

    #ASP setting

---
+++ Controllers/GetDate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("getdate")]
+    public class GetDate : ControllerBase
+    {
+        private readonly ILogger<GetDate> _logger;
+
+        public GetDate(ILogger<GetDate> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet( Name = "CalculateGetDate")]
+        public ActionResult<int> GetDateDifference(string date)
+        {
+            if (DateTime.TryParse(date, out DateTime parsedDate))
+            {
+                var currentDate = DateTime.Now;
+                var difference = (currentDate - parsedDate).Days;
+                return (difference);
+            }
+            else
+            {
+                return BadRequest("Please enter correct date");
+            }
+        }
+
+        [HttpGet("calculateAverage", Name = "GetAverage")]
+        public ActionResult<double> GetAverage([FromQuery] List<int> numbers)
+        {
+            double average = numbers.Average();
+            return (average);
+        }
+
+        
+    }
+}
---
+++ Controllers/RecordTime.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("time")]
+    public class RecordTime : ControllerBase
+    {
+        private readonly ILogger<RecordTime> _logger;
+
+        public RecordTime(ILogger<RecordTime> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCurrentTime")]
+        public ActionResult<string> GetCurrentTime()
+        {
+            var currentTime = DateTime.Now.ToString("HH:mm:ss");
+            return (currentTime);
+        }
+    }
+}
+
